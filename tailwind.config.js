a6cf865fb798890a4709b96cfae0b73c192448f2
yjkim <qlrogo91lp@gmail.com>
2025-05-05 17:44:19 +0900

    🔧 font 설치, theme 설정, font size 설정

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      body: ["Noto Sans KR"],
+    },
+    fontSize: {
+      xs: [
+        "12px",
+        {
+          lineHeight: "18px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      sm: [
+        "14px",
+        {
+          lineHeight: "21px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      base: [
+        "16px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      xl: ["20px", "30px"],
+      "2xl": [
+        "24px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+    },
+  },
+  plugins: [],
+};
